b627ae49a18539ea17897d4371c0ac4bab5c8889
Saurav Singh <sauravcloud26@gmail.com>
2025-05-21 22:31:43 +0530

    made changes in the routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,41 @@ import HomePage from "./page/HomePage";
 import LoginPage from "./page/LoginPage";
 import SignUpPage from "./page/SignUpPage";
 import { useAuthStore } from "./store/useAuthStore";
+import Layout from "./layout/Layout";
+import { Loader } from "lucide-react";
 
 const App = () => {
-  // const authUser = useAuthStore((state) => state.authUser);
-  // const checkAuth = useAuthStore((state) => state.checkAuth);
-  let { authUser, checkAuth } = useAuthStore();
+  const { isCheckingAuth, authUser, checkAuth } = useAuthStore();
+
+  // On first load, check if token exists
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
+  // While checking auth, show loader
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center justify-start">
       <Routes>
-        <Route
-          path="/"
-          element={authUser != null ? <HomePage /> : <Navigate to={"/login"} />}
-        />
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+        </Route>
+
         <Route
           path="/login"
           element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
         />
+
         <Route
           path="/signup"
           element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
