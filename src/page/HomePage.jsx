7ea58c94cd1bd11863f2b922399b85f032a5cf2e
Saurav Singh <sauravcloud26@gmail.com>
2025-05-21 22:31:23 +0530

    added logout, navbar and home page base

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
 const HomePage = () => {
+  const { isCheckingAuth,authUser,checkAuth } = useAuthStore()
   return (
     <div>
       homepage
