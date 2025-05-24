855de53a4a116c9b1f462a1e45b1c2887374f9ac
Saurav Singh <sauravcloud26@gmail.com>
2025-05-24 17:55:14 +0530

    Added basic home page and addProblem

---
+++ src/page/AddProblem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateProblemForm from "../component/CreateProblemForm";
+
+const AddProblem = () => {
+  return (
+    <div className="w-full">
+      <CreateProblemForm />
+    </div>
+  );
+};
+
+export default AddProblem;
