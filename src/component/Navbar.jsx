b9ad3b7795de3f6d2685b21f26db4e4b3a74a221
Saurav Singh <sauravcloud26@gmail.com>
2025-05-31 18:57:06 +0530

    link bug fixed on clicking app pic

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -11,10 +11,9 @@ const Navbar = () => {
     <nav className="sticky top-0 z-50 w-full py-5">
       <div className="flex w-full justify-between mx-auto max-w-4xl bg-black/15 shadow-lg shadow-neutral-600/5 backdrop-blur-lg border border-gray-200/10 p-4 rounded-2xl">
         {/* Logo Section */}
-        <Link to="/" className="flex items-center gap-3 cursor-pointer">
           <Logo width={150} height={60} />
           <span className="text-lg md:text-2xl font-bold tracking-tight text-white hidden md:block"></span>
-        </Link>
+
 
         {/* User Profile and Dropdown */}
         <div className="flex items-center gap-8">
